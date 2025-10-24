843a551834987d4601da9efcd1d33d04ee991552
Lucas-Mosca <110690274+Lucas-Mosca@users.noreply.github.com>
2025-10-24 11:24:27 -0300

    Feature: Create splash screen and home redirect.

--- app/src/main/java/br/com/moscatech/dumbpassword/ui/screens/splash/SplashFragment.kt
+++ app/src/main/java/br/com/moscatech/dumbpassword/ui/screens/splash/SplashFragment.kt
@@ -2,8 +2,11 @@ package br.com.moscatech.dumbpassword.ui.screens.splash
 
 import android.view.LayoutInflater
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavOptions
+import br.com.moscatech.dumbpassword.R
 import br.com.moscatech.dumbpassword.base.BaseFragment
 import br.com.moscatech.dumbpassword.databinding.FragmentSplashBinding
+import br.com.moscatech.dumbpassword.utils.navigate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,9 +17,21 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
     override val viewModel: SplashViewModel by viewModels()
 
-    override fun initViews() {
-    }
+    override fun initViews() {}
 
     override fun initObservers() {
+        viewModel.showHome.observe(this) {
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.splashFragment, inclusive = true)
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_in_left)
+                .setPopExitAnim(R.anim.slide_out_right)
+                .build()
+
+            val direction = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
+
+            navigate(direction, navOptions)
+        }
     }
 }
