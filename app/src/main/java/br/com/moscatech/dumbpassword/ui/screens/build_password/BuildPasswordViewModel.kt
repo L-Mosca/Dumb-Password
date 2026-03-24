121bec88511dc440ce0b4aa27a664eaad4969593
Lucas-Mosca <110690274+Lucas-Mosca@users.noreply.github.com>
2026-03-23 23:30:31 -0300

    Feature: Create CreatePassword screen.

---
+++ app/src/main/java/br/com/moscatech/dumbpassword/ui/screens/build_password/BuildPasswordViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.moscatech.dumbpassword.ui.screens.build_password
+
+import br.com.moscatech.dumbpassword.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BuildPasswordViewModel @Inject constructor(): BaseViewModel() {
+}
--- app/src/main/java/br/com/moscatech/dumbpassword/ui/screens/create_password/CreatePasswordFragment.kt
+++ app/src/main/java/br/com/moscatech/dumbpassword/ui/screens/create_password/CreatePasswordFragment.kt
@@ -49,6 +49,19 @@ class CreatePasswordFragment : BaseFragment<FragmentCreatePasswordBinding>() {
                     CreatePasswordFragmentDirections.actionCreatePasswordFragmentToPlatformNavGraph()
                 navigate(direction, navOptions)
             }
+
+            dcvPassword.setOnClickListener {
+                val navOptions = NavOptions.Builder()
+                    .setEnterAnim(R.anim.slide_in_right)
+                    .setExitAnim(R.anim.slide_out_left)
+                    .setPopEnterAnim(R.anim.slide_in_left)
+                    .setPopExitAnim(R.anim.slide_out_right)
+                    .build()
+
+                val direction =
+                    CreatePasswordFragmentDirections.actionCreatePasswordFragmentToPasswordNacGraph()
+                navigate(direction, navOptions)
+            }
         }
     }
 
